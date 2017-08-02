7679b77902508f1a23fd5a103ca62e526792ed58
SylveonBottle <depressedeevee@gmail.com>
2017-08-02 14:07:46 -0500

    Purge more than just users

--- 98-purge-users.sh
+++ 98-purge-users.sh
@@ -4,11 +4,24 @@
 #	Will delete all catbot- users and their home directories
 #
 
-read -p "Press ENTER to continue"
-
-for i in {1..12}
+if [ $EUID == 0 ]; then
+	echo "This script must not be run as root"
+	exit
+fi
+echo "Doing this will delete all catbot users and folders."
+read -p "Are you sure?"
+for n in $(ls -1 /home | grep cat | wc -l)
 do
-	echo "Deleting user catbot-$i"
-	sudo userdel -r catbot-$i
-	sudo groupdel catbot-$i
+	for u in $(seq 1 $n)
+	do
+		echo "Deleting user catbot-$u"
+		sudo userdel -r catbot-$u
+		sudo groupdel catbot-$u
+		done
+done
+
+for p in $(ls -1 /home | grep catbot-)
+do
+	echo "Removing leftover folder $p"
+	sudo rm -rf /home/$p
 done
