a903efe63177e00b60f86e0e4211ad8472fda40b
SylveonBottle <depressedeevee@gmail.com>
2019-06-21 02:47:49 -0500

    Use global indentions

--- convert.js
+++ convert.js
@@ -16,29 +16,54 @@ var accountsStr = [];
 var array = fs.readFileSync("accounts.txt").toString().replace(/\n\n/g, "\n").split("\n");
 array.pop();
 for (i in array) {
-	accountsStr.push(array[i]);
+        <<<<<<< HEAD
+    accountsStr.push(array[i]);
+        =======
+        accounts_str.push(array[i]);
+        >>>>>>> parent of 081eb83... Leading spaces to tabs
 }
 // Keep track of processed accs and their respective database indexes
 var processedAccs = 0;
 var dbIdx = 0;
 
 // Loop accs and convert to proper format
+    <<<<<<< HEAD
 accountsStr.forEach(function (i, idx, arr) {
-	var json = JSON.parse("{\"login\": \"\",\"password\": \"\",\"email\": \"@inboxkitten.com\",\"community\": true,\"privacy\":{\"profile\": 3,\"comments\": 3,\"inventory\": 3,\"gameDetails\": 3},\"avatar\": \"https://i.imgur.com/avatar.png\",\"summary\": \"summary\",\"username\": \"\",\"created\": 1533672324,\"steamID\": \"1234567891234567\"}");
-	json.login = i.split(":")[0];
-	json.username = json.login;
-	json.password = i.split(":")[1].replace(/(\r\n|\n|\r)/gm, "");
-	json.email = json.login.toLowerCase() + "@inboxkitten.com";
-	accounts["array"].push(json);
-	processedAccs++;
-	if (processedAccs >= 1000) {
-		processedAccs = 0;
-		fs.writeFileSync(`accounts${dbIdx}.cg.json`, JSON.stringify(accounts), "utf8");
-		dbIdx++;
-		accounts = JSON.parse("{ \"array\": [], \"used\": 0 }");
-	}
+    var json = JSON.parse("{\"login\": \"\",\"password\": \"\",\"email\": \"@inboxkitten.com\",\"community\": true,\"privacy\":{\"profile\": 3,\"comments\": 3,\"inventory\": 3,\"gameDetails\": 3},\"avatar\": \"https://i.imgur.com/avatar.png\",\"summary\": \"summary\",\"username\": \"\",\"created\": 1533672324,\"steamID\": \"1234567891234567\"}");
+    json.login = i.split(":")[0];
+    json.username = json.login;
+    json.password = i.split(":")[1].replace(/(\r\n|\n|\r)/gm, "");
+    json.email = json.login.toLowerCase() + "@inboxkitten.com";
+    accounts["array"].push(json);
+    processedAccs++;
+    if (processedAccs >= 1000) {
+        processedAccs = 0;
+        fs.writeFileSync(`accounts${dbIdx}.cg.json`, JSON.stringify(accounts), "utf8");
+        dbIdx++;
+        accounts = JSON.parse("{ \"array\": [], \"used\": 0 }");
+    }
 });
 // Write last few accs
 if (processedAccs) {
-	fs.writeFileSync(`accounts${dbIdx}.cg.json`, JSON.stringify(accounts), "utf8");
+    fs.writeFileSync(`accounts${dbIdx}.cg.json`, JSON.stringify(accounts), "utf8");
 }
+    =======
+    accounts_str.forEach(function (i, idx, arr) {
+        var json = JSON.parse('{"login": "","password": "","email": "@inboxkitten.com","community": true,"privacy":{"profile": 3,"comments": 3,"inventory": 3,"gameDetails": 3},"avatar": "https://i.imgur.com/avatar.png","summary": "summary","username": "","created": 1533672324,"steamID": "1234567891234567"}');
+        json.login = i.split(":")[0];
+        json.username = json.login;
+        json.password = i.split(":")[1].replace(/(\r\n|\n|\r)/gm, "");;
+        json.email = json.login.toLowerCase() + "@inboxkitten.com";
+        accounts['array'].push(json);
+        processed_accs++;
+        if (processed_accs >= 1000) {
+            processed_accs = 0;
+            fs.writeFileSync(`accounts${db_idx}.cg.json`, JSON.stringify(accounts), "utf8");
+            db_idx++;
+            accounts = JSON.parse('{ "array": [], "used": 0 }');
+        }
+    });
+// Write last few accs
+if (processed_accs)
+    fs.writeFileSync(`accounts${db_idx}.cg.json`, JSON.stringify(accounts), "utf8");
+    >>>>>>> parent of 081eb83... Leading spaces to tabs
